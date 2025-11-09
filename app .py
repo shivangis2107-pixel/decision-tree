16b149641d1d11249c823cb154d5e23d382406c8
shivangis2107-pixel <shivangis2107@gmail.com>
2025-11-09 13:34:59 +0530

    Update app .py

--- app .py
+++ app .py
@@ -1,9 +1,9 @@
 import streamlit as st
 import pandas as pd
-import joblib
+import pickle
 
 filename = 'dtmodel.pkl'
-loaded_model = joblib.load(open(filename, 'rb'))
+loaded_model = pickle.load(open(filename, 'rb'))
 
 # Define the correct column names
 columns = ['Delivery_Distance', 'Traffic_Congestion', 'Weather_Condition',
